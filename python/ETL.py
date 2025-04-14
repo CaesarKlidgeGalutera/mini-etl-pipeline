2f830d92b145f83830aa971abb1eadc4434dd840
Caesar Klidge Galutera <caesarklidgegalutera@gmail.com>
2025-04-14 22:01:38 +0800

    transform data code and uploading in s3 using lambda

--- python/ETL.py
+++ python/ETL.py
@@ -1,17 +1,48 @@
 import requests
 from dotenv import load_dotenv
 import os 
+import boto3
+import json
 
 load_dotenv()
-API_KEY = os.getenv("OpenWeathetMap_API_KEY")
+API_KEY = os.getenv("OpenWeatherMap_API_KEY")
 
-city = 'Pasig'
-url =  f"https://api.openweathermap.org/data/2.5/weather?q={city}&appid={API_KEY}"
+S3_BUCKET_NAME = os.getenv("S3_BUCKET_NAME")
 
-# Extract the data from the OpenWeatherMap API
-response = requests.get(url)
-json_data = response.json()
-print(json_data)
+s3 = boto3.client('s3')
 
-#Transform the data
+def lambda_handler(event, context):
+    # Extract the data from the OpenWeatherMap API
+    city = 'Pasig'
+    url =  f"https://api.openweathermap.org/data/2.5/weather?q={city}&appid={API_KEY}"
+    
+    # Extract the data from the OpenWeatherMap API
+    response = requests.get(url)
+    json_data = response.json()
+    
+    # Transform the data
+    weather = {
+        'city': json_data['name'],
+        'country': json_data['sys']['country'],
+        'temperature': json_data['main']['temp'],
+        'humidity': json_data['main']['humidity'],
+        'pressure': json_data['main']['pressure'],
+        'wind_speed': json_data['wind']['speed'],
+        }
+    
+    file_name = f"{weather['city']}_{weather['country']}.json"
+    
+    #Load the data into S3 bucket
+    s3.put_object(
+        Bucket=S3_BUCKET_NAME,
+        Key=file_name,
+        Body=json.dumps(weather),
+        ContentType='application/json'
+    )
+    
+    return {
+        'statusCode': 200,
+        'body': f"Weather data for {weather['city']} has been uploaded to S3 bucket {S3_BUCKET_NAME}"
+    }
 
+print(lambda_handler(None, None))
