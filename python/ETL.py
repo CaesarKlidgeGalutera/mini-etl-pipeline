c1781c797162336ea71e69d4bad0072b0281c48d
Caesar Klidge Galutera <caesarklidgegalutera@gmail.com>
2025-04-17 20:34:44 +0800

    Added Hadling Errors

--- python/ETL.py
+++ python/ETL.py
@@ -3,6 +3,7 @@ from dotenv import load_dotenv
 import os 
 import boto3
 import json
+from botocore.exceptions import ClientError, BotoCoreError
 
 load_dotenv()
 API_KEY = os.getenv("OpenWeatherMap_API_KEY")
@@ -20,29 +21,71 @@ def lambda_handler(event, context):
     response = requests.get(url)
     json_data = response.json()
     
+    try:
     # Transform the data
-    weather = {
-        'city': json_data['name'],
-        'country': json_data['sys']['country'],
-        'temperature': json_data['main']['temp'],
-        'humidity': json_data['main']['humidity'],
-        'pressure': json_data['main']['pressure'],
-        'wind_speed': json_data['wind']['speed'],
+        weather = {
+            'city': json_data['name'],
+            'country': json_data['sys']['country'],
+            'temperature': json_data['main']['temp'],
+            'humidity': json_data['main']['humidity'],
+            'pressure': json_data['main']['pressure'],
+            'wind_speed': json_data['wind']['speed'],
+            }
+        
+        file_name = f"{weather['city']}_{weather['country']}.json"
+        
+        
+        #Load the data into S3 bucket
+        s3.put_object(
+            Bucket=S3_BUCKET_NAME,
+            Key=file_name,
+            Body=json.dumps(weather),
+            ContentType='application/json'
+        )
+        
+        #Return Message Upload Success
+        return {
+            'statusCode': 200,
+            'body': f"Weather data for {weather['city']} has been uploaded to S3 bucket {S3_BUCKET_NAME}"
+        }
+        
+    #Return HTTPError from WeatherAPI
+    except requests.exceptions.HTTPError as e:
+        return {
+            'statusCode': response.status_code,
+            'body':f"Weather API HTTP error: {str(e)}"
         }
     
-    file_name = f"{weather['city']}_{weather['country']}.json"
+    #Return Requests Error
+    except requests.exceptions.RequestException as e:
+        return {
+            'statusCode': 500,
+            'body': f"Weather API request failed: {str(e)}"
+        }
     
-    #Load the data into S3 bucket
-    s3.put_object(
-        Bucket=S3_BUCKET_NAME,
-        Key=file_name,
-        Body=json.dumps(weather),
-        ContentType='application/json'
-    )
+    #Return ClientError from S3
+    except ClientError as e:
+        return {
+            'statusCode': 500,
+            'body':f"S3 ClientError: {e.response['Error']['Message']}"
+        }
     
-    return {
-        'statusCode': 200,
-        'body': f"Weather data for {weather['city']} has been uploaded to S3 bucket {S3_BUCKET_NAME}"
-    }
+    #Return low-level Boto3 SDK issues
+    except BotoCoreError as e:
+        return {
+            'statusCode': 500,
+            'body':f"S3 BotoCoreError: {str(e)}"
+        }
+    
+    #Return other kinds of Error
+    except Exception as e:
+        return {
+            'statusCode': 500,
+            'body':f"Unexpected error: {str(e)}"
+        }
+
+    
+
+
 
 print(lambda_handler(None, None))
